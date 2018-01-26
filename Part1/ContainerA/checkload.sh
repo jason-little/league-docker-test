09ec4ede40499a1c8f4437cedd2b6040a20921a8
jason-little <shattered_lands@hotmail.com>
2018-01-26 14:55:46 -0500

    modifying email address in checkload

--- Part1/ContainerA/checkload.sh
+++ Part1/ContainerA/checkload.sh
@@ -2,7 +2,7 @@
  
 load=`echo $(cat /proc/loadavg | awk '{print $1}') \> 3 | bc -l`
 if [ "$load" -ne 0 ]; then
-        echo "Your Server Load Alert Needs Attention! " | mail -s "System Load Alert $load" shattered_lands@hotmail.com 
+        echo "Your Server Load Alert Needs Attention! " | mail -s "System Load Alert $load" take-home-test@league.pagerduty.com 
 
         echo "Alert email sent to take-home-test@league.pagerduty.com"
 fi
